1a04108d8640523a2050d28542b1b152c5c64970
MadleenH <57007358+MadleenH@users.noreply.github.com>
2019-11-21 18:53:53 +0100

    Initial commit

---
+++ Assets/Scenes/scripts/DestroyOutOfBoundsRL.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBoundsRL : MonoBehaviour
+{
+    private float rightBound = -60;
+    private float leftBound = 45;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > leftBound){
+            Destroy(gameObject);
+        }
+        else if (transform.position.x < rightBound){
+            Destroy(gameObject);
+            }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Shooter
+ topdown shooter
